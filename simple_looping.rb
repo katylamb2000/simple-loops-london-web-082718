75bb9199f8ee431e477ea5cdbbca0c886c484e61
katy@karstaway.com <katy@karstaway.com>
2018-08-24 06:20:19 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -39,3 +39,11 @@ puts phrase
 counter += 1
 end
 end
+
+def for_iterator(number_of_times)
+
+  phrase = "Welcome to Flatiron School's Web Development Course!"
+   for number in 0..number_of_times do
+     puts phrase
+  end
+end
